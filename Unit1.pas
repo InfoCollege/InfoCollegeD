8f20fe99487d7756723fd4d9a93d6e580333ca3a
InfoCollege <avantipov@mgkit.ru>
2016-11-12 10:28:28 -0800

    Начало разработки (12.11.2016)
    Начало разработки

---
+++ Unit1.pas
@@ -0,0 +1,58 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, jpeg, ExtCtrls, DB, ADODB;
+
+type
+  TForm1 = class(TForm)
+    Image1: TImage;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Label5: TLabel;
+    Label6: TLabel;
+    Button1: TButton;
+    ADOConnection1: TADOConnection;
+    ADOQuery1: TADOQuery;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+uses Unit2;
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+ADOQuery1.Close;
+ADOQuery1.SQL.Clear;
+ADOQuery1.SQL.Add('SELECT count(*)');
+ADOQuery1.SQL.Add('FROM Сотрудники');
+ADOQuery1.SQL.Add('WHERE Логин='''+Edit1.Text+'''');
+ADOQuery1.SQL.Add('AND Пароль='''+Edit2.Text+''';');
+ADOQuery1.Open;
+//showmessage(ADOQuery1.SQL.text);
+if ADOQuery1.FieldCount = 1 then
+begin
+Form1.Hide;
+Form2.show;
+end
+else
+showmessage('Ошибка №1.Обнаруженая неправильная пара логин/пароль');
+end;
+
+end.
---
+++ Unit2.pas
@@ -0,0 +1,47 @@
+unit Unit2;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, Menus, ComCtrls, StdCtrls, jpeg, ExtCtrls;
+
+type
+  TForm2 = class(TForm)
+    Image1: TImage;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    MonthCalendar1: TMonthCalendar;
+    MainMenu1: TMainMenu;
+    N1: TMenuItem;
+    N3: TMenuItem;
+    N4: TMenuItem;
+    N5: TMenuItem;
+    N6: TMenuItem;
+    N7: TMenuItem;
+    N8: TMenuItem;
+    N2: TMenuItem;
+    N9: TMenuItem;
+    N10: TMenuItem;
+    N11: TMenuItem;
+    N12: TMenuItem;
+    N13: TMenuItem;
+    N14: TMenuItem;
+    N15: TMenuItem;
+    N16: TMenuItem;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+end.
