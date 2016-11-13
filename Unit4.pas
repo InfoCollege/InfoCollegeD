ac1725174209140af929021b0038abb02b81ebde
InfoCollege <avantipov@mgkit.ru>
2016-11-13 13:55:59 -0800

    13.11.2016
    Модуль "Регистрация дипломов" -подключен и активен

--- Unit4.pas
+++ Unit4.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, jpeg, ExtCtrls, DBCtrls;
+  Dialogs, StdCtrls, jpeg, ExtCtrls, DBCtrls, DB, ADODB, Grids, DBGrids;
 
 type
   TForm4 = class(TForm)
@@ -27,7 +27,12 @@ type
     Edit7: TEdit;
     ListBox1: TListBox;
     Label9: TLabel;
+    Label10: TLabel;
+    DBGrid1: TDBGrid;
+    ADOQuery1: TADOQuery;
+    DataSource1: TDataSource;
     procedure ListBox1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -38,6 +43,8 @@ var
   Form4: TForm4;
 
 implementation
+
+uses Unit1;
 
 {$R *.dfm}
 
@@ -57,6 +64,18 @@ if ListBox1.Selected[4]= true then
 Edit7.Text:='Специалист по земельно-имущественным отношениям';
 if ListBox1.Selected[5]= true then
 Edit7.Text:='Специалист по рекламе';
+end;
+
+procedure TForm4.Button2Click(Sender: TObject);
+begin
+Form1.ADOQuery1.Close;
+Form1.ADOQuery1.SQL.Clear;
+Form1.ADOQuery1.SQL.Add('INSERT INTO Дипломы');
+Form1.ADOQuery1.SQL.Add('VALUES ('+Edit1.text+','+Edit2.Text+','+Edit3.text+',');
+Form1.ADOQuery1.SQL.Add(''+Edit4.Text+','+Edit5.Text+','+Edit6.Text+'');
+Form1.ADOQuery1.SQL.Add(','+Listbox1.Items[ListBox1.ItemIndex]+','+Edit7.text+',');
+//Form1.ADOQuery1.SQL.Add(''+DBGrid1.DataSource.DataSet.Fields.Fields[0].Value+''');');
+showmessage(Form1.ADOQuery1.SQL.Text);
 end;
 
 end.
